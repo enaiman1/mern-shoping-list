f4ae21a81625969c89178faf65e9ab88a4c85c79
Ericcentrik <enaiman1@gmail.com>
2020-05-27 11:29:11 -0400

    started working on Reducer and Action for Auth and reducer for error handling

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+import { 
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+ } from "./types";
+
+//  check token & load user
+export const loadUser = () => (dispatch, getState) => {
+    //User Loading
+    dispatch({type: USER_LOADING});
+    // get token from local storage
+    const token = getState().auth.token
+
+    // Headers
+    const config ={
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    // if token, add to headers
+    if(token) {
+        config.headers['x-auth-token'] = token;
+    }
+    axios.get('/api/auth/user', config)
+    .then(res => dispatch({
+        type: USER_LOADED,
+        payload: res.data
+    }))
+    .catch(err => {
+        dispatch({
+            type: AUTH_ERROR
+        });
+    })
+}
